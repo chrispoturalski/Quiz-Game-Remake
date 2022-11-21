ac8be426cbec46102fd0bad70c27c99e8e31d1df
Chris Poturalski <ChrisPoturalski@chriss-mbp.lan>
2022-11-21 10:58:29 -0800

    adding some functions and cleaning up some code

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,28 +1,83 @@
 // Variables from HTML
 var header = document.querySelector('.header')
-var header = document.querySelector('.highscoreBtn')
+var highscore = document.querySelector('.highscoreBtn')
 var content = document.getElementById('content')
 var score = document.getElementById('game-score')
-var timer = document.getElementById('.timer')
+var setTimer = document.getElementById('setTimer')
 var gameArea = document.querySelector('.game')
 var questionArea = document.getElementById('question-area')
 var questionText = document.getElementById('question-text')
 var answerChoices = document.getElementById('answer-choices')
-var optionOne = doucment.getElementById('option-one')
-var optionTwo = doucment.getElementById('option-two')
-var optionThree = doucment.getElementById('option-three')
-var startGame = document.getElementById('start-game')
+var optionOne = document.getElementById('option-one')
+var optionTwo = document.getElementById('option-two')
+var optionThree = document.getElementById('option-three')
+var startBtn = document.getElementById('start-game')
 
 //Questions array
 var questions = 
 [
     {
         question: "What is 3x1?",
-        answers: [
-            {option: "0",answer:false},
-            {option: "3",answer:true},
-            {option: "4",answer:false},
-        ]
+        answers: ["1", "3", "4"],
+        answer: 1
     },
 ]
-test
+
+// created game variables
+var currentQuestion = 0;
+var gameRunning = false;
+var timeLeft = 60;
+var interval;
+
+//Call a function to deduct time if question is missed
+function deductTime() {
+    timeLeft -= 10;
+}
+
+// Start game function
+var startGame = function () {
+    if (gameRunning) return;
+    console.log("Starting the game, good luck!!!")
+    gameRunning = true;
+    currentQuestion = 0;
+    startTimer();
+    displayQuestions(questions[currentQuestion]);
+    startBtn.style.display = "none";
+}
+
+
+//We need a function to start the timer
+function startTimer() {
+    setTimer.textContent = timeLeft + ' Seconds Remaining';
+    timer = setInterval(() => {
+        timeLeft--;
+        setTimer.textContent = timeLeft + ' Seconds Remaining';
+        //When the time runs out, we need to stop the game and end the game
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 100);
+}
+
+//Function to display the questions
+function displayQuestions(question){
+    console.log(questions[0]);
+    questionText.innerHTML = questions[currentQuestion].question;
+    optionOne.innerHTML = questions[currentQuestion].answers[0];
+    optionTwo.innerHTML = questions[currentQuestion].answers[1];
+    optionThree.innerHTML = questions[currentQuestion].answers[2];
+}
+
+
+//Function to call when the game is over
+var endGame = function () {
+    console.log("Game over!!!")
+    gameRunning = false;
+    timeLeft = 60;
+    //highscoreDisplay();
+}
+
+// Create event listener for game
+startBtn.addEventListener('click', startGame);
+// highscore.addEventListener('click', highscores);
